789f041dad73ea09be0584545b35ed921084a584
Claudio Cortese <claudio.cortese@>
2024-10-28 18:46:08 +0100

    Various improvements

---
+++ index.js
@@ -0,0 +1,13 @@
+document.addEventListener('DOMContentLoaded', () => {
+    const showDetailsBtn = document.querySelectorAll('button.btn.show-details');
+    const showLessDetailsBtn = document.querySelectorAll('.show-less-details');
+
+    [...showDetailsBtn, ...showLessDetailsBtn].forEach((button) => {
+        console.log(button);
+        
+        button.addEventListener('click', ({target}) => {
+            console.log(target);
+            target.closest('.card-holder').classList.toggle('flip');
+        })
+    })
+})
